d4395fa100aa0ebab392df6861cfe3c5b0a610b5
cr4z <stevencr7zz@gmail.com>
2025-08-20 17:59:13 -0500

    Resolve app-name reference issue

--- obj/Debug/net9.0/AzureURLShortener.AssemblyInfo.cs
+++ obj/Debug/net9.0/AzureURLShortener.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureURLShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2775dd0443bbe861994a7de4c74d8f3fa8a9f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureURLShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureURLShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
